c86ea8c7170c5abf4cbec239fa89668dabf6adf0
Omar <hammado92@gmail.com>
2023-10-26 15:36:40 +0300

    upload react demo

---
+++ Demos/04 JavaScript/React/todo/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  const subject = props.subject
+  console.log(props)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello {subject}!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
